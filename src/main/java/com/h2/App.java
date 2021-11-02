ca9290ddfb647c8a64c83b7c397bd0b843abdb21
ASelcuk61 <aselcuk2110@gmail.com>
2021-11-01 23:17:49 -0400

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,33 +4,25 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 
     public static int doubleTheNumber(int number) {
-<<<<<<< HEAD
 
-        return number*2;
-    }
-    private static int add(int[] numbers){
-        int sum=0;
-        for(int i=0;i<numbers.length;i++){
-            sum+=numbers[i];
-=======
-        return 2 * number;
+        return number * 2;
     }
 
     private static int add(int[] numbers) {
         var sum = 0;
-        for(int number: numbers) {
-            sum += number;
->>>>>>> 6e0b52974cd87da99219317bbd50f810c99f50e8
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+
         }
         return sum;
+
+
     }
 }
