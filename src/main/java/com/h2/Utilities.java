1d3466ed2099ec16a94ec2f641568b0678fb33f6
ASelcuk61 <aselcuk2110@gmail.com>
2021-11-11 20:18:27 -0500

    complete module

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -19,7 +19,7 @@ public class Utilities {
             out=Integer.parseInt(in);
         }
         catch (NumberFormatException e) {
-          throw  new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
+          throw  new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
         }
         return out;
     }
