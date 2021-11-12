3690ca47dc05441651ec4ca3676f0a692f758dd1
ASelcuk61 <aselcuk2110@gmail.com>
2021-11-11 20:00:41 -0500

    complete module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
 import java.util.Map;
+import com.h2.BestLoanRates;
+import com.h2.SavingsCalculator;
+import com.h2.MortgageCalculator;
 public class Finance {
 
     public final static String BEST_LOAN_RATES="bestLoanRates";
@@ -21,7 +25,39 @@ public class Finance {
     }
 
     private static void executeCommand(String command, String[] arguments){
-        
+        switch(command){
+            case BEST_LOAN_RATES: System.out.println("Finding best loan rates ...");
+            BestLoanRates.main(arguments);
+            return;
+            case SAVINGS_CALCULATOR: System.out.println("Finding your net savings ...");
+                SavingsCalculator.main(arguments);
+                return;
+            case MORTGAGE_CALCULATOR:System.out.println("Finding your monthly payment ...");
+                MortgageCalculator.main(arguments);
+                return;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String command=args[0];
+       if( !commandsToUsage.containsKey(command)){
+           System.out.println(command+": command not found");
+           return;
+       }
+
+       boolean isValidCommand=validateCommandArguments(args);
+       if (!isValidCommand) {
+           System.out.println(commandsToUsage.get(args[0]));
+           return;
+       }
+       executeCommand(command, Arrays.copyOfRange(args,1,args.length));
+
+
+
+
+
+
     }
 
 
